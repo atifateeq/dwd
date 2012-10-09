6c6f1c3c405a6a5d1cd78812059feb052c51a72d
Atif Ahmad <TheBoss@172-26-15-34.dynapool.nyu.edu>
2012-10-09 13:48:03 -0400

    test

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 import os, datetime
 import re
 from unidecode import unidecode
@@ -13,35 +12,73 @@ from mongoengine import *
 import models
 
 app = Flask(__name__)   # create our flask app
+app.config['CSRF_ENABLED'] = False
 
 # --------- Database Connection ---------
 # MongoDB connection to MongoLab's database
 connect('mydata', host=os.environ.get('MONGOLAB_URI'))
-print "Connecting to MongoLabs"
+app.logger.debug("Connecting to MongoLabs")
 
-
+# hardcoded categories for the checkboxes on the form
 categories = ['web','physical computing','software','video','music','installation','assistive technology','developing nations','business','social networks']
 
 # --------- Routes ----------
 
 # this is our main page
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def index():
-	# render the template, pass in the animals dictionary refer to it as 'animals'
-	templateData = {
-		'ideas' : models.Idea.objects(),
-		'categories' : categories
-	}
-	return render_template("main.html", **templateData)
 
+	# get Idea form from models.py
+	idea_form = models.IdeaForm(request.form)
+	
+	# if form was submitted and it is valid...
+	if request.method == "POST" and idea_form.validate():
+	
+		# get form data - create new idea
+		idea = models.Idea()
+		idea.creator = request.form.get('creator','anonymous')
+		idea.title = request.form.get('title','no title')
+		idea.slug = slugify(idea.title + " " + idea.creator)
+		idea.idea = request.form.get('idea','')
+		idea.categories = request.form.getlist('categories') # getlist will pull multiple items 'categories' into a list
+		
+		idea.save() # save it
+
+		# redirect to the new idea page
+		return redirect('/ideas/%s' % idea.slug)
+
+	else:
+
+		# for form management, checkboxes are weird (in wtforms)
+		# prepare checklist items for form
+		# you'll need to take the form checkboxes submitted
+		# and idea_form.categories list needs to be populated.
+		if request.method=="POST" and request.form.getlist('categories'):
+			for c in request.form.getlist('categories'):
+				idea_form.categories.append_entry(c)
+
+		# render the template
+		templateData = {
+			'ideas' : models.Idea.objects(),
+			'categories' : categories,
+			'form' : idea_form
+		}
+
+		return render_template("main.html", **templateData)
+
+# Display all ideas for a specific category
 @app.route("/category/<cat_name>")
 def by_category(cat_name):
 
+	# try and get ideas where cat_name is inside the categories list
 	try:
 		ideas = models.Idea.objects(categories=cat_name)
+
+	# not found, abort w/ 404 page
 	except:
 		abort(404)
 
+	# prepare data for template
 	templateData = {
 		'current_category' : {
 			'slug' : cat_name,
@@ -51,27 +88,9 @@ def by_category(cat_name):
 		'categories' : categories
 	}
 
+	# render and return template
 	return render_template('category_listing.html', **templateData)
 
-@app.route("/ideas/add", methods=["POST"])
-def newidea():
-	
-	app.logger.debug('idea form response data')
-	app.logger.debug(request.form)
-	app.logger.debug('list of submitted categories')
-	app.logger.debug(request.form.getlist('categories'))
-
-	# get form data - create new idea
-	idea = models.Idea()
-	idea.creator = request.form.get('creator','anonymous')
-	idea.title = request.form.get('title','no title')
-	idea.slug = slugify(idea.title + " " + idea.creator)
-	idea.idea = request.form.get('idea','')
-	idea.categories = request.form.getlist('categories')
-	
-	idea.save()
-
-	return redirect('/ideas/%s' % idea.slug)
 
 @app.route("/ideas/<idea_slug>")
 def idea_display(idea_slug):
@@ -89,7 +108,55 @@ def idea_display(idea_slug):
 
 	# render and return the template
 	return render_template('idea_entry.html', **templateData)
+
+@app.route("/ideas/<idea_slug>/edit", methods=['GET','POST'])
+def idea_edit(idea_slug):
+
 	
+	# try and get the Idea from the database / 404 if not found
+	try:
+		idea = models.Idea.objects.get(slug=idea_slug)
+		
+		# get Idea form from models.py
+		# if http post, populate with user submitted form data
+		# else, populate the form with the database record
+		idea_form = models.IdeaForm(request.form, obj=idea)	
+	except:
+		abort(404)
+
+	# was post received and was the form valid?
+	if request.method == "POST" and idea_form.validate():
+	
+		# get form data - update a few fields
+		# note we're skipping the update of slug (incase anyone has previously bookmarked)
+		idea.creator = request.form.get('creator','anonymous')
+		idea.title = request.form.get('title','no title')
+		idea.idea = request.form.get('idea','')
+		idea.categories = request.form.getlist('categories')
+
+		idea.save() # save changes
+
+		return redirect('/ideas/%s/edit' % idea.slug)
+
+	else:
+
+		# for form management, checkboxes are weird (in wtforms)
+		# prepare checklist items for form
+		# you'll need to take the form checkboxes submitted
+		# and idea_form.categories list needs to be populated.
+		if request.method=="POST" and request.form.getlist('categories'):
+			for c in request.form.getlist('categories'):
+				idea_form.categories.append_entry(c)
+
+		templateData = {
+			'categories' : categories,
+			'form' : idea_form,
+			'idea' : idea
+		}
+
+		return render_template("idea_edit.html", **templateData)
+
+
 @app.route("/ideas/<idea_id>/comment", methods=['POST'])
 def idea_comment(idea_id):
 
@@ -116,6 +183,8 @@ def idea_comment(idea_id):
 	
 	# append comment to idea
 	idea.comments.append(comment)
+
+	# save it
 	idea.save()
 
 	return redirect('/ideas/%s' % idea.slug)
--- models.py
+++ models.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from mongoengine import *
+
+from flask.ext.mongoengine.wtf import model_form
 from datetime import datetime
+
+# our demo model from week 5 in class
+class Log(Document):
+	text = StringField()
+	timestamp = DateTimeField(default=datetime.now())
 
 class Comment(EmbeddedDocument):
 	name = StringField()
@@ -9,10 +16,10 @@ class Comment(EmbeddedDocument):
 
 class Idea(Document):
 
-	creator = StringField(max_length=120, required=True)
+	creator = StringField(max_length=120, required=True, verbose_name="First name")
 	title = StringField(max_length=120, required=True)
 	slug = StringField()
-	idea = StringField(required=True)
+	idea = StringField(required=True, verbose_name="What is your idea?")
 
 	# Category is a list of Strings
 	categories = ListField(StringField(max_length=30))
@@ -22,5 +29,10 @@ class Idea(Document):
 
 	# Timestamp will record the date and time idea was created.
 	timestamp = DateTimeField(default=datetime.now())
+
+
+# Create a Validation Form from the Idea model
+IdeaForm = model_form(Idea)
+
 	
 
