927afa50d27bdd5bf9b6d1792c2d4c6e5e1e9517
Atif Ahmad <TheBoss@172-26-12-233.dynapool.nyu.edu>
2012-11-12 16:45:11 -0500

    initial commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/TheBoss/Desktop/ITP/DWD/database/venv"
+set -gx VIRTUAL_ENV "/Users/TheBoss/Desktop/DWD/database/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
